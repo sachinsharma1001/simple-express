6eb062502301db39d82a8755cc93b125820cb390
sachinsharma1001 <sachin.sharma100197@gmail.com>
2020-12-05 19:14:59 +0530

    port change

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
+app.listen(8084, () => {
+  console.log("Server running on port 8084");
 });
 
 app.get("/health", (req, res, next) => {
